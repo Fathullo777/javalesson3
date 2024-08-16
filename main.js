da1773f60422fb2fcb022a4e6b51112360823b00
Fathullo <fathullorasulov198@gmail.com>
2024-08-16 13:25:16 +0500

    final

---
+++ main.js
@@ -0,0 +1,63 @@
+let ser = prompt('введит число бананов')
+
+
+let i = 1
+      do {
+            console.log(i + ' banana');
+            
+            
+      }while (i>ser);
+      do {
+            console.log(i + ' bananas');
+            i++
+            
+      }while (i<=ser);
+
+            
+
+
+
+
+
+
+
+
+
+
+
+let sj = prompt('введите число чтобы найти сумму четных чисел')
+let sum = 0
+for (let i = 0; i<=sj;i++) 
+    if (i%2==0) 
+      sum+=i;
+console.log(sum);
+alert('Посмотрите ответ в консоли')
+
+
+
+
+
+let a = prompt('введите число');
+let b = prompt('введите степень')
+alert('число- '+ a +
+      ' степень- ' + b +
+      ' ответ- ' + a**b
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+    
+    
+
